d53402b8331b1eb0c5006d77d80fc6ba08e343ac
rockysingh9431 <rockysingh943189@gmail.com>
2023-05-10 01:36:53 +0530

    api.py changes

--- api.py
+++ api.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 def apiApp():
-	account_sid = "AC3eac6945f78f6d01c69ca0f6e57d9317"
-	auth_token = "760dffb099028531973ac9252f2d09aa"
+	account_sid = "**************************"
+	auth_token = "***************************"
 	client = Client(account_sid, auth_token)
 	message = client.messages.create(
 	  body="Accident is Detected",
